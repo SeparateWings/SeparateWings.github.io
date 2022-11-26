76e5959bccf1d89da1dcb079998810d440e6ea02
SeparateWings <jiayaozon@163.com>
2022-11-26 21:53:28 +0800

    Change button function to change pictures

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
+let myButton = document.querySelector('button');
 let myImage = document.querySelector('img');
 
-myImage.onclick = function () {
+function changePicture() {
     let MySrc = myImage.getAttribute('src');
     if (MySrc === 'images/kokomi.jpg') {
         myImage.setAttribute('src', 'images/jk_kokomi.jpg');
@@ -13,28 +14,35 @@ myImage.onclick = function () {
     }
 }
 
-let myButton = document.querySelector('button');
-let MyHeading = document.querySelector('h1');
-
-function setWifeName() {
-    let wifeName = prompt('请输入我的老婆名字。')
-    if (!wifeName) {
-        setWifeName();
-    }
-    else {
-        localStorage.setItem('name', wifeName);
-        MyHeading.textContent = '我的老婆' + wifeName;
-    }
+myImage.onclick = function () {
+    changePicture();
 }
-
-if (!localStorage.getItem('name')) {
-    setWifeName();
-}
-else {
-    let storedName = localStorage.getItem('name');
-    MyHeading.textContent = '我的老婆' + storedName;
-}
-
 myButton.onclick = function () {
-    setWifeName();
+    changePicture();
 }
+
+// let myButton = document.querySelector('button');
+// let MyHeading = document.querySelector('h1');
+
+// function setWifeName() {
+//     let wifeName = prompt('请输入我的老婆名字。')
+//     if (!wifeName) {
+//         setWifeName();
+//     }
+//     else {
+//         localStorage.setItem('name', wifeName);
+//         MyHeading.textContent = '我的老婆' + wifeName;
+//     }
+// }
+
+// if (!localStorage.getItem('name')) {
+//     setWifeName();
+// }
+// else {
+//     let storedName = localStorage.getItem('name');
+//     MyHeading.textContent = '我的老婆' + storedName;
+// }
+
+// myButton.onclick = function () {
+//     setWifeName();
+// }
